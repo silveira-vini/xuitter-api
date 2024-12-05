1b5b5dc4e235ed06e59aa8e7e365e91833f63265
silveira-vini <vsribeiro87@gmail.com>
2024-12-05 10:17:27 -0300

    inicial comit

---
+++ src/test/java/br/com/xuitter/xuitter_api/XuitterApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.xuitter.xuitter_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XuitterApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
